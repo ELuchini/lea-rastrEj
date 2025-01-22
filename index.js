5a66ebd6ff11993c0831264037360a7732a4ed45
ELuchini <eluchini@outlook.com>
2025-01-22 01:44:19 +0000

    Item 6-16 ok

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+app.use(cors());
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
+database = [];
 
+app
+  .post("/api/users", (req, res) => {
+    let username = req.body.username;
+    let userId = (database.length + 7777777).toString();
+    let newUser = {
+      username: username,
+      _id: userId,
+    };
 
+    database.push(newUser);
+    res.json(newUser);
+  })
+  .get("/api/users", (req, res) => {
+    res.json(database);
+  });
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log("Your app is listening on port " + listener.address().port);
+});
